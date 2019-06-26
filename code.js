8b9583b81573064a50327cf37717b06402ac2dae
il-hubspot <48827131+il-hubspot@users.noreply.github.com>
2019-06-26 14:27:45 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,37 +1,14 @@
 jQuery(document).ready(function($) {
-    $(".header-3-v2").each(function() {
+    $(".header-4-v2").each(function() {
         var o = $(this);
-        
         if (o.hasClass("init")) {
             return;
         }
-
         o.addClass("init");
-
-        var headerId = "id-" + Math.random().toString().slice(2);
-        o.attr("id", headerId);
       
-        var style = "";
+      	var style = "";
 
-        o.find(".js-nav-styles").each(function(){
-            var parentMenuColor = $( this ).data( 'nav-parent-color' );
-            var parentMenuHoverColor = $( this ).data( 'nav-parent-hover-color' );
-            var childMenuColor = $( this ).data( 'nav-child-color' );
-            var childMenuHoverColor = $( this ).data( 'nav-child-hover-color' );
-            var childMenuBgColor = $( this ).data( 'nav-child-bg' );
-            
-            var navId = "id-" + Math.random().toString().slice(2);
-            $(this).attr("id", navId);
-
-            style += "#" + navId + " ul > li > a { color: " + parentMenuColor + "; } \n";
-          	style += "#" + navId + " ul > li > a:hover, #" + navId + " ul > li > a:focus, #" + navId + " ul > li > a:active { color: " + parentMenuHoverColor + "; } \n";
-            style += "#" + navId + " ul > li > .hs-menu-children-wrapper li a { color: " + childMenuColor + "; } \n";
-          	style += "#" + navId + " ul > li > .hs-menu-children-wrapper li a:hover, #" + navId + " ul > li > .hs-menu-children-wrapper li a:focus, #" + navId + " ul > li > .hs-menu-children-wrapper li a:active { color: " + childMenuHoverColor + "; } \n";
-            style += "#" + navId + " ul > li > .hs-menu-children-wrapper { background-color: " + childMenuBgColor + "; } \n";
-            style += "#" + headerId + " .navbar-toggle .icon-bar { background-color: " + parentMenuColor + "; } \n";
-        });
-      
-      	o.find(".js-btn-color").each(function(){
+        o.find(".js-btn-color").each(function(){
             var btnType = $(this).data('btn-type');
             var btnColor = $(this).data('btn-color');
             var btnHoverColor = $(this).data('btn-hover-color');
