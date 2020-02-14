eb587a77cc4a64febee1d10a037b6be65001170a
il-hubspot <48827131+il-hubspot@users.noreply.github.com>
2020-02-14 13:07:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,26 +1,20 @@
 jQuery(document).ready(function($) {
-    $(".content-13-v2").each(function() {
+    $(".content-16-v2").each(function() {
         var o = $(this);
+        
         if (o.hasClass("init")) {
             return;
         }
+
         o.addClass("init");
+
+        var id = "id-" + Math.random().toString().slice(2);
+        o.attr("id", id);
       
-      	var style = "";
-      
-      	var newId = "id-" + Math.random().toString().slice(2);
-        o.attr("id", newId);
-      
-      	var btnColor = $(this).find(".js-form-btn-color").data("form-btn-color");
-        if (btnColor && btnColor !== "#16a085") {
-            style += "#" + newId + ".content-13 .hs_submit input[type='submit'] { background-color: " + btnColor + "; } \n";
-            $("<style type='text/css'/>").html(style).appendTo("head");
-        }
-      
-      	var btnHoverColor = $(this).find(".js-form-btn-color").data("form-btn-hover-color");
-        if (btnHoverColor && btnHoverColor !== "#24deb4") {
-            style += "#" + newId + ".content-13 .hs_submit input[type='submit']:hover, #" + newId + ".content-13 .hs_submit input[type='submit']:focus, #" + newId + ".content-13 .hs_submit input[type='submit']:active{ background-color: " + btnHoverColor + "; } \n";
-            $("<style type='text/css'/>").html(style).appendTo("head");
-        }
+      	o.find('.control-btn.fui-arrow-down').click(function(e) {
+        	e.preventDefault();
+
+        	$("html, body").animate({scrollTop: $(this).closest(".hs_cos_wrapper").next().offset().top});
+      	});
     });
 });
